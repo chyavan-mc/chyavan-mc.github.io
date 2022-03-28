05919dd823b2bca1c07cf7735f1e4a6e0d5020f5
Chyavan M C <Chyavan.M.C@wdc.com>
2022-03-28 17:22:37 +0530

    Navbar initial

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+const ids = {
+    navbar: "LWCp",
+    nav_sections: {
+        "home": "ZQJw",     // Home
+        "about": "bIdh",     // About
+        "projects": "ably",     // Projects
+        // "contact": "6c6f",     // Contact
+    },
+    scroll: "8f78"
+}
+
+function selectDeselectNavBar(id) {
+    if(id == null) {
+        id = ids.nav_sections.home;
+    }
+    Object.values(ids.nav_sections).forEach(e => {
+        var htmlElem = document.getElementById(e);
+        if(htmlElem){
+            if (e == id) {
+                htmlElem.classList.add("navbar-a-active");
+                htmlElem.classList.remove("navbar-a");
+            }
+            else {
+                htmlElem.classList.remove("navbar-a-active");
+                htmlElem.classList.add("navbar-a");
+            }
+        }
+    });
+}
