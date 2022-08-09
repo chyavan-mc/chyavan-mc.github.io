c834667874f3d2716b30c1ba31e98d6a86ddebbe
Chyavan M C <chyavan.mc4@gmail.com>
2022-08-09 10:58:57 -0500

    section 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,11 @@ const ids = {
         "qualifications": "ably",   // Projects
         // "contact": "6c6f",     // Contact
     },
-    scroll: "8f78"
+    scroll: "8f78",
+    qualifications: {
+        "5nIr": "I1bI",
+        "2dO1": "INmn"
+    }
 }
 
 function selectDeselectNavBar(id) {
@@ -23,6 +27,33 @@ function selectDeselectNavBar(id) {
             else {
                 htmlElem.classList.remove("navbar-a-active");
                 htmlElem.classList.add("navbar-a");
+            }
+        }
+    });
+}
+
+function selectDeselectQualification(id) {
+    Object.keys(ids.qualifications).forEach(e => {
+        var htmlElem = document.getElementById(ids.qualifications[e]);
+        if(htmlElem){
+            if (e == id) {
+                htmlElem.classList.remove("d-none");
+            }
+            else {
+                htmlElem.classList.remove("d-none");
+                htmlElem.classList.add("d-none");
+            }
+        }
+
+        var htmlElem = document.getElementById(e);
+        if(htmlElem){
+            if (e == id) {
+                htmlElem.classList.remove("single-tab-inactive");
+                htmlElem.classList.add("single-tab-active");
+            }
+            else {
+                htmlElem.classList.remove("single-tab-active");
+                htmlElem.classList.add("single-tab-inactive");
             }
         }
     });
