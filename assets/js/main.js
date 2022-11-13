061e52a99164f95c782e48bed764e05f578a02c8
Chyavan M C <chyavan.mc4@gmail.com>
2022-11-12 22:08:44 -0600

    Qualifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,10 +7,6 @@ const ids = {
         // "contact": "6c6f",     // Contact
     },
     scroll: "8f78",
-    qualifications: {
-        "5nIr": "I1bI",
-        "2dO1": "INmn"
-    }
 }
 
 function selectDeselectNavBar(id) {
@@ -27,33 +23,6 @@ function selectDeselectNavBar(id) {
             else {
                 htmlElem.classList.remove("navbar-a-active");
                 htmlElem.classList.add("navbar-a");
-            }
-        }
-    });
-}
-
-function selectDeselectQualification(id) {
-    Object.keys(ids.qualifications).forEach(e => {
-        var htmlElem = document.getElementById(ids.qualifications[e]);
-        if(htmlElem){
-            if (e == id) {
-                htmlElem.classList.remove("d-none");
-            }
-            else {
-                htmlElem.classList.remove("d-none");
-                htmlElem.classList.add("d-none");
-            }
-        }
-
-        var htmlElem = document.getElementById(e);
-        if(htmlElem){
-            if (e == id) {
-                htmlElem.classList.remove("single-tab-inactive");
-                htmlElem.classList.add("single-tab-active");
-            }
-            else {
-                htmlElem.classList.remove("single-tab-active");
-                htmlElem.classList.add("single-tab-inactive");
             }
         }
     });
